c37af46095f33a371b226eb30b68d05c4eef2cfb
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-27 19:26:21 +0530

    Degtofaren

---
+++ m1/DegtoFaren/DegToFaren.java
@@ -0,0 +1,8 @@
+class DegToFaren {
+
+    public static void main(String[] args) {
+    	int deg = 32;
+        float faren = (9*deg)/5+32;
+	    System.out.println("Degrees "+deg+" in faren is "+faren);
+}
+}
