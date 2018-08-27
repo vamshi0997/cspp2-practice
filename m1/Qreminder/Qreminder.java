5876776599aaa91ef5ffa73ef9b1da77a862423e
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-27 19:22:25 +0530

    Qrem

---
+++ m1/Qreminder/Qreminder.java
@@ -0,0 +1,9 @@
+public class Qreminder{
+	public static void main(String args[]){
+	int a = 4, b = 2;
+	int quotient = a/b;
+	int reminder = a%b;
+	System.out.println("quotient =" + quotient);
+	System.out.println("reminder =" + reminder);
+	}
+}
