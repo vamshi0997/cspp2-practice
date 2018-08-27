01dc034e348db64644951100bacc7a231de970c6
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-27 19:05:20 +0530

    assign1

---
+++ m1/Assignment-1/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		System.out.print("enter name:");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.print("enter Rollnumber:");
+        String Rollnumber = sc.nextLine();
+		System.out.println("Name : , Roll Number : " + name + " " + Rollnumber);
+	}
+}
