3ebb0bdf5fcdabcbba57c6ea7fce9eb9d5e8a1c8
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-28 14:51:32 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -10,22 +10,22 @@ public class Solution {
     /*
     scanner class is used to take a, b, c input
     */
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation function and
-	print the output.
-	*/
-	static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
-	/*
-	finding roots for equation
-	*/
-		double root1 = (-b + Math.sqrt(b * b - (4 * a * c))) / (2 * a);
-		double root2 = (-b - Math.sqrt(b * b - (4 * a * c))) / (2 * a);
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation function and
+    print the output.
+    */
+    static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
+    /*
+    finding roots for equation
+    */
+        double root1 = (-b + Math.sqrt(b * b - (4 * a * c))) / (2 * a);
+        double root2 = (-b - Math.sqrt(b * b - (4 * a * c))) / (2 * a);
         System.out.println(root1 + " " + root2);
-	}
+    }
 }
