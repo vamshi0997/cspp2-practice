e7d38f1c862219b261a6d19cf2f4f9e932acb127
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-28 16:16:34 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     /*
     Do not modify this main function.
     */
-    public static void main(final String[] args) {
+    public static void main (final String[] args) {
     /*
     scanner class is used to take a, b, c input
     */
@@ -19,8 +19,7 @@ public class Solution {
     Need to write the rootsOfQuadraticEquation function and
     print the output.
     */
-    static void rootsOfQuadraticEquation(final int a, final int b, final int c)
-    {
+    static void rootsOfQuadraticEquation (final int a, final int b, final int c) {
     /*
     finding roots for equation
     */
