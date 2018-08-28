b217f28855530bd02f95e31e5cdccc5d95c0d22b
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-28 14:49:27 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -5,8 +5,11 @@ import java.lang.Math.*;
 public class Solution {
     /*
     Do not modify this main function.
-	*/
-	public static void main(final String[] args) {
+    */
+    public static void main(final String[] args) {
+    	/*
+    	scanner class is used to take a, b, c input
+    	*/
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
@@ -18,6 +21,9 @@ public class Solution {
 	print the output.
 	*/
 	static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
+		/*
+		finding roots for equation
+		*/
 		double root1 = (-b + Math.sqrt(b * b - (4 * a * c))) / (2 * a);
 		double root2 = (-b - Math.sqrt(b * b - (4 * a * c))) / (2 * a);
         System.out.println(root1 + " " + root2);
