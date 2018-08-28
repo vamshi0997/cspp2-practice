b25eccf2d5cf6fb6b073932403722d952b5df8b0
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-28 17:49:43 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m2/Assignment-3/Solution.java
+++ m2/Assignment-3/Solution.java
@@ -28,8 +28,7 @@ public class Solution {
     static long power(final int base, final int exponent) {
         if (exponent == 0) {
             return 1;
-            }
-        else {
+            } else {
             return base * power(base, (exponent - 1));
             }
     }
