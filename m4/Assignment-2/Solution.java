b26ffea3cd59b8a890c950630bc976711e7dc7fe
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-30 19:53:09 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -17,12 +17,21 @@ public final class Solution {
         Scanner sc = new Scanner(System.in);
         int rows1 = sc.nextInt();
         int columns1 = sc.nextInt();
-        int[][] matrix1 = matrix_(rows1, columns1);
+        int[][] matrix1 = new int[rows1][columns1];
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < columns1; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
 
         int rows2 = sc.nextInt();
         int columns2 = sc.nextInt();
-        int[][] matrix2 = matrix_(rows2, columns2);
-        
+        int[][] matrix2 = new int[rows2][columns2];
+        for (int i = 0; i < rows2; i++) {
+            for (int j = 0; j < columns2; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
 
         int[][] arr = new int[rows1][columns1];
         if (rows1 == rows2 && columns1 == columns2) {
@@ -35,15 +44,5 @@ public final class Solution {
         } else {
                 System.out.println("not possible");
           }
-    }
-    static int[][] matrix_(int rows, int columns){
-        Scanner s = new Scanner(System.in);
-        int[][] matrix1 = new int[rows][columns];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                matrix1[i][j] = s.nextInt();
-            }
-        }
-        return matrix1;
     }
 }
