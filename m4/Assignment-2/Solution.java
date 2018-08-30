a05a1fe1b26e12a89a131e2060f9a46200612475
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-30 19:08:12 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -18,8 +18,8 @@ public class Solution {
 		int rows1 = sc.nextInt();
 		int columns1 = sc.nextInt();
 		int[][] matrix1 = new int[rows1][columns1];
-		for (int i = 0; i < rows1; i ++) {
-	    	for (int j = 0; j < columns1; j ++) {
+		for (int i = 0; i < rows1; i++) {
+	    	for (int j = 0; j < columns1; j++) {
 	    		matrix1[i][j] = sc.nextInt();
 	    	}
 	    }
@@ -28,15 +28,15 @@ public class Solution {
 		int columns2 = sc.nextInt();
 		int[][] matrix2 = new int[rows2][columns2];
 	    for (int i = 0; i < rows2; i ++) {
-	    	for (int j = 0; j < columns2; j ++) {
+	    	for (int j = 0; j < columns2; j++) {
 	    		matrix2[i][j] = sc.nextInt();
 	    	}
 	    }
 
 	    int[][] arr = new int[rows1][columns1];
 	    if (rows1 == rows2 && columns1 == columns2) {
-            for(int i = 0; i < rows1; i ++) {
-                for (int j = 0; j < columns1; j ++) {
+            for(int i = 0; i < rows1; i++) {
+                for (int j = 0; j < columns1; j++) {
                     arr[i][j] = matrix1[i][j] + matrix2[i][j];
                 }
             System.out.println(arr[i][0] + " " + arr[i][1] + " " + arr[i][2]);
