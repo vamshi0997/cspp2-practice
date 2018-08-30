982e9c506a44ab4af8c39aa6b0a1a6a97762ba68
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-30 14:28:55 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -1,34 +1,33 @@
-import java.util.*;
-/** 
- * @details program to find the largest number in array.
+import java.util.Scanner;
+/**
  * @author vamshi
  */
 public final class Solution {
-	/**
-	 * @brief creating default constructor for class
-	 * @default constructor
-	 */
-	private Solution() {
+    /**
+     * @brief creating default constructor for class
+     * @default constructor
+     */
+    private Solution() {
 
-	}
+    }
     /**
      * @details Fill this main function to print maximum of given array
      * @param args string
      */
 
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n= sc.nextInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-		    arr[i] = sc.nextInt();	
-		}
-		int max = 0;
-	    for (int j = 0; j < arr.length; j++) {
-	    	if (arr[j] > max) {
-	    		max = arr[j];
-	    	}
-	    }
-	    System.out.println(max);
-	}
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = 0;
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] > max) {
+                max = arr[j];
+            }
+        }
+        System.out.println(max);
+    }
 }
