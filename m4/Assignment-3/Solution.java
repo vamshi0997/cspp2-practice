3e254500a9730831efb075d5383d70787e92b680
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-31 13:43:22 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -30,7 +30,7 @@ public class Solution
         	n = n/10;
         	count1 += 1;
         }
-        String sum1 = Double.toString(sum);
+        String sum1 = Double.toString((int)sum);
         return sum1;
 	}
 
