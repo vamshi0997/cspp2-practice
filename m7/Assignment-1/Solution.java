a5d009357fd13ccd12bb0f582ef0c8e3b9599a41
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-09-03 17:08:31 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -1,12 +1,12 @@
 import java.util.*;
 import java.lang.*;
 /**
- * @details To find string length is greater than 6.
  * @author vamshi
  */
 class InputValidator {
     /*Write the atrributes and methods for InputValidator*/
     String str1;
+    int length = str1.length();
     final int six = 6;
     /**
      * @param str string
@@ -18,7 +18,7 @@ class InputValidator {
      * @return boolean
      */
     public boolean validateData() {
-        if (str1.length() >= six) {
+        if (length >= six) {
             return true;
         } else {
             return false;
@@ -40,10 +40,10 @@ public final class Solution {
      * In main method.
      * @param args string
      */
-    public static void main(final String args[]) {
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         String input = s.next();
-        InputValidator i = new InputValidator(input);       
+        InputValidator i = new InputValidator(input);
         System.out.println(i.validateData());
 
     }
