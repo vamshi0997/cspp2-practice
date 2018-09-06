4d459ebdc56a8b6f85beb56043d75999edc7e554
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-09-06 14:50:11 +0530

    "check_style_score -> 5 of 6 passed. style score: 0/1 "

--- m10/Assignment-3/List.java
+++ m10/Assignment-3/List.java
@@ -156,7 +156,7 @@ public class List<E> {
      */
     public boolean contains(E item) {
 		//Write logic for contains method
-        return indexOf(item) == -1;
+        return indexOf(item) != -1;
     }
     /*
      * Returns the index of the first occurrence 
