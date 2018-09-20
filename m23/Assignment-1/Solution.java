1ec2c2c748bfbf0113ae462223f8cd388f974121
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-09-20 20:25:38 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- m23/Assignment-1/Solution.java
+++ m23/Assignment-1/Solution.java
@@ -1,39 +1,48 @@
-import java.io.*;
+import java.util.regex.*;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 
+class BagOfWords {
 
+}
+
+class StringMatching {
+
+}
 
 class Solution {
-
-	public static void main(String[] args) {
+    
+    public static void main(String[] args) {
+        
+        String path = "/Users/apple/cspp2-practice/m23/Assignment-1/Test/";
 		Scanner scan = new Scanner(System.in);
 		String text = scan.nextLine();
-		if (text == null) {
-			System.out.println("empty directory");
-		} else if (text == "Test") {
-			File folder = new File("/Users/apple/cspp2-practice/m23/Assignment-1/Test");
-			fileopen(folder);
-
-		} else if (text == "Test2") {
-			File folder = new File("/Users/apple/cspp2-practice/m23/Assignment-1/Test2");
-            fileopen(folder);
-		} else {
-			File folder = new File("/Users/apple/cspp2-practice/m23/Assignment-1/Test3");
-            fileopen(folder);
-		}
+			if (text.equals("")) {
+				System.out.println("empty directory");
+			}
+			else {
+				File folder = new File("/Users/apple/cspp2-practice/m23/Assignment-1/" + text);
+			    fileopen(folder, path);
+			}
 	}
-
-		static void fileopen(File folder) {
-			File[] listOfFiles = folder.listFiles();
-            for (File file : listOfFiles) {
-			    if (file.isFile()) {
-				    System.out.println(file.getName());
-			    }
-		    }
-		}
+		
+    
+    public static void fileopen(File folder, String path) {
+    	
+    	String currentLine="";
+	    File[] listOfFiles = folder.listFiles();
+        if (listOfFiles.length == 0) {
+        	System.out.println("empty directory");
+        }
+	}
 }
