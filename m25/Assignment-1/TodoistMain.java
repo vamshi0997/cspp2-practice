9bc7f0d6e16f24d3ab2dc9c809486178bef1a466
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-09-22 11:23:43 +0530

    "check_style_score -> 3 of 14 passed. style score: 0/1 "

--- m25/Assignment-1/TodoistMain.java
+++ m25/Assignment-1/TodoistMain.java
@@ -14,13 +14,25 @@ class Task {
     boolean important;
     boolean urgent;
     String status;
-    Task (String title1, String assignedTo1, int timeToComplete1, boolean important1, boolean urgent1, String status1) {
-        this.title = title1;
+    Task(String title1, String assignedTo1, int timeToComplete1, boolean important1, boolean urgent1, String status1) throws Exception {
+        if (title1 == null) {
+            throw new Exception("no title");
+        } else {
+            this.title = title1;
+        }
+        if (timeToComplete1 < 0) {
+            throw new Exception("no time");
+        } else {
+            this.timeToComplete = timeToComplete1;
+        }
+        if (status1.equals("todo") || status1.equals("done")) {
+            this.status = status1;
+        } else {
+            throw new Exception("no status");
+        }
         this.assignedTo = assignedTo1;
-        this.timeToComplete = timeToComplete1;
         this.important = important1;
         this.urgent = urgent1;
-        this.status = status1;
     }
 
     String getImportant() {
@@ -57,8 +69,14 @@ public class TodoistMain {
             String[] tokens = s.nextLine().split(",");
             switch (tokens[0]) {
                 case "task":
+                try {
                     testTask(tokens);
-                break;
+                } catch(Exception e) {
+                    System.out.println(e);
+                }
+                // case "task":
+                //     testTask(tokens);
+                // break;
                 // case "add-task":
                 //     testAddTask(todo, tokens);
                 // break;
